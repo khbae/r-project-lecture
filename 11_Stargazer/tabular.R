2467f10fa400ae9b69ffa690cd716d307969e121
khbae <kbae2010@gmail.com>
2020-12-11 07:17:04 +0900

    stargazer

---
+++ 11_Stargazer/tabular.R
@@ -0,0 +1,13 @@
+library(dplyr)
+library(stargazer)
+out = stargazer(iris)
+is(out)
+
+tabular <- function(stargazer.out,file.name){
+  out = head(stargazer.out[-1:-6],-1) 
+  cat(paste(out2, collapse="\n"),sep="\n",file = file.name)
+}
+
+iris %>% 
+  stargazer(.) %>%
+  tabular(.,"fn.tex")
